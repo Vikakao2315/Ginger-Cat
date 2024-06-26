750aa1a5455b7ee6350ab8439d57a63c9c1bc6ac
v.ianina <v.ianina@dataduck.com>
2024-06-26 20:30:17 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <h1>Адаптивный Лендинг</h1>
+                <p>Добро пожаловать на наш сайт!</p>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
